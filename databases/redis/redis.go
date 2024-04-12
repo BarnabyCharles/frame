3886ebccd2fbc0a2c15e40341226bf4a2b149757
dupengyi <17737913867@qq.com>
2024-04-12 17:27:30 +0800

    添加redis功能

--- databases/redis/redis.go
+++ databases/redis/redis.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-func WithRedis(address string, hand func(cli *redis.Client) (string, error)) (string, error) {
+func WithRedis(address string, hand func(cli *redis.Client) (interface{}, error)) (interface{}, error) {
 
 	err := config.ViperInit(address)
 	if err != nil {
@@ -28,6 +28,17 @@ func WithRedis(address string, hand func(cli *redis.Client) (string, error)) (st
 		Addr: r.Redis.Host + ":" + r.Redis.Port,
 		DB:   int(r.Redis.DB),
 	})
+	// 设置AOF同步方式为always，每次写入都同步
+	_, err = cli.ConfigSet(context.Background(), "appendfsync", "always").Result()
+	if err != nil {
+		panic(err)
+	}
+
+	// 等待AOF文件同步完成
+	_, err = cli.Do(context.Background(), "CLIENT", "REPLY", "ON").Result()
+	if err != nil {
+		panic(err)
+	}
 
 	defer cli.Close()
 
@@ -39,7 +50,7 @@ func WithRedis(address string, hand func(cli *redis.Client) (string, error)) (st
 }
 
 func SetByVal(address, key string, val interface{}, duration time.Duration) error {
-	_, err := WithRedis(address, func(cli *redis.Client) (string, error) {
+	_, err := WithRedis(address, func(cli *redis.Client) (interface{}, error) {
 		err := cli.Set(context.Background(), key, val, duration).Err()
 		if err != nil {
 			return "", err
@@ -53,7 +64,7 @@ func SetByVal(address, key string, val interface{}, duration time.Duration) erro
 }
 
 func GetKey(address, key string) (string, error) {
-	val, err := WithRedis(address, func(cli *redis.Client) (string, error) {
+	val, err := WithRedis(address, func(cli *redis.Client) (interface{}, error) {
 		val, err := cli.Get(context.Background(), key).Result()
 		if err != nil {
 			return "", err
@@ -63,18 +74,18 @@ func GetKey(address, key string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return val, nil
+	return val.(string), nil
 }
 
 func Exists(address, key string) bool {
-	ok, _ := WithRedis(address, func(cli *redis.Client) (string, error) {
+	ok, _ := WithRedis(address, func(cli *redis.Client) (interface{}, error) {
 		ok, err := cli.Exists(context.Background(), key).Result()
 		if err != nil {
 			panic(err)
 		}
 		return strconv.FormatInt(ok, 10), nil
 	})
-	if ok == "1" {
+	if ok.(string) == "1" {
 		return true
 
 	}
@@ -82,7 +93,7 @@ func Exists(address, key string) bool {
 }
 
 func KeyDelete(address, key string) error {
-	_, err := WithRedis(address, func(cli *redis.Client) (string, error) {
+	_, err := WithRedis(address, func(cli *redis.Client) (interface{}, error) {
 		err := cli.Del(context.Background(), key).Err()
 		return "", err
 	})
@@ -95,7 +106,7 @@ func KeyDelete(address, key string) error {
 
 func Lock(address, key string, val interface{}, duration time.Duration, isReadOnly bool) (bool, error) {
 	res := false
-	WithRedis(address, func(cli *redis.Client) (string, error) {
+	WithRedis(address, func(cli *redis.Client) (interface{}, error) {
 		if !isReadOnly {
 			for {
 				result, err := cli.SetNX(context.Background(), key, val, duration).Result()
@@ -119,11 +130,65 @@ func Lock(address, key string, val interface{}, duration time.Duration, isReadOn
 }
 
 func UnLock(address, key string) error {
-	_, err := WithRedis(address, func(cli *redis.Client) (string, error) {
+	_, err := WithRedis(address, func(cli *redis.Client) (interface{}, error) {
 		return "", cli.Del(context.Background(), key).Err()
 	})
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func SetHashValue(address, key, field string, value interface{}) error {
+	_, err := WithRedis(address, func(cli *redis.Client) (interface{}, error) {
+		err := cli.HSet(context.Background(), key, field, value).Err()
+		return "", err
+	})
+	return err
+}
+
+func GetHashValue(address, key, field string) (interface{}, error) {
+	withRedis, err := WithRedis(address, func(cli *redis.Client) (interface{}, error) {
+		return cli.HGet(context.Background(), key, field).Result()
+	})
+	if err != nil {
+		return err, nil
+	}
+
+	return withRedis, err
+}
+
+func GetKeysInfos(address, key string) ([]string, error) {
+	withRedis, err := WithRedis(address, func(cli *redis.Client) (interface{}, error) {
+		return cli.HKeys(context.Background(), key).Result()
+	})
+	if err != nil {
+		return nil, err
+	}
+	return withRedis.([]string), nil
+}
+
+func DecrBy(address, key string, val int64) (int64, error) {
+	data, err := WithRedis(address, func(cli *redis.Client) (interface{}, error) {
+		return cli.DecrBy(context.Background(), key, val).Result()
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	return data.(int64), nil
+
+}
+
+func IncrBy(address, key string, val int64) (int64, error) {
+	data, err := WithRedis(address, func(cli *redis.Client) (interface{}, error) {
+		return cli.IncrBy(context.Background(), key, val).Result()
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	return data.(int64), nil
 }
